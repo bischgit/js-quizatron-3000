90f606281735f53eabbc4d47e5c637e185068d0d
BrendanBischoff <101281767+bischgit@users.noreply.github.com>
2022-05-28 06:39:27 -0400

    added array with quiz questions in js file

---
+++ assets/quiz.js
@@ -0,0 +1,29 @@
+// Var with array and object for questions 
+var questions = [
+    {
+        title: "Commonly used data types DO NOT include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
+    },
+    {
+        title: "Which of the following is a Javascript Operator?",
+        choices: ["space bar", "?", "flux capacitor", "=="],
+        answer: "=="
+    },
+    {
+        title: "Arrays in Javascript can be used to store ____.",
+        choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+        answer: "all of the above"
+    },
+    {
+        title: "String values must be enclosed within ____ when being assigned to variables.",
+        choices: ["commas", "curly brackets", "quotes", "parentheses"],
+        answer: "quotes"
+    },
+    {
+        title: "A very useful tool for used during development and debugging for printing content to the debugger is:",
+        choices: ["Javascript", "terminal / bash", "for loops", "console log"],
+        answer: "console log"
+    },
+
+];
